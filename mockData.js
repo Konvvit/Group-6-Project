e997906c1a17fcad632a27b60cbebf24de5bcd15
Roudi22 <johndoe@example.com>
2023-12-13 15:41:05 +0100

    Mock data javascript file

---
+++ mockData.js
@@ -0,0 +1,104 @@
+const products = [
+    {
+        id: 1,
+        name: "Kyckling med grönsaker",
+        price: 130,
+        image: "image/1.PNG",
+        valuta: "$",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "kyckling"
+    },
+    {
+        id: 2,
+        name: "Kyckling Vingar",
+        price: 150,
+        image: "image/2.PNG",
+        valuta: "kr",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "kyckling"
+    },
+    {
+        id: 3,
+        name: "Fisk med grönsaker",
+        price: 170,
+        image: "image/3.PNG",
+        valuta: "kr",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "fisk"
+    },
+    {
+        id: 4,
+        name: "Fisk med Broccoli",
+        price: 180,
+        image: "image/4.PNG",
+        valuta: "$",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "fisk"
+    },
+    {
+        id: 5,
+        name: "Kött med morötter",
+        price: 150,
+        image: "image/5.PNG",
+        valuta: "$",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "kött"
+    },
+    {
+        id: 6,
+        name: "Steak med sås",
+        price: 170,
+        image: "image/6.png",
+        valuta: "$",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "steak"
+    },
+    {
+        id: 7,
+        name: "Steak med sallad",
+        price: 180,
+        image: "image/7.PNG",
+        valuta: "$",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "steak"
+    },
+    {
+        id: 8,
+        name: "Ribeye steak",
+        price: 190,
+        image: "image/8.PNG",
+        valuta: "kr",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "steak"
+    },
+    {
+        id: 9,
+        name: "Friterad Kyckling med pomme",
+        price: 180,
+        image: "image/9.png",
+        valuta: "$",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "kyckling"
+    },
+    {
+        id: 10,
+        name: "Snacks",
+        price: 150,
+        image: "image/10.webp",
+        valuta: "kr",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "snacks"
+    },
+    {
+        id: 11,
+        name: "Kött med potatis",
+        price: 180,
+        image: "image/11.PNG",
+        valuta: "kr",
+        beskrivning: "lorem ipsum nontojsdkfn sdkf ksdpof",
+        category: "kött"
+    }
+    
+]
+
+export default products;
